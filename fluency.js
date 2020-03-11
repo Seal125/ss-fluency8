e2323fb7d8bc892d82427cd03e27caedba116136
EC2 Default User <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2020-03-11 20:16:38 +0000

    finished fluency

--- fluency.js
+++ fluency.js
@@ -1,1 +1,27 @@
+const requestMethod = (method,url,data) =>{
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: data ? {'Content-Type': 'application/json'} : {}
+  })
+};
 
+const getData = async () => {
+  const data = await requestMethod('GET', 'https://jsonplaceholder.typicode.com/users');
+  const response = await data.json();
+  return response
+}
+
+const paintDOM = async() =>{
+  const users = await getData();
+    
+  users.forEach(user => {
+    document.getElementById('tbody').innerHTML +=`
+     <tr>
+        <td>${user.name}</td>
+        <td>${user.company.name}</td>
+      </tr>
+    `
+  })
+}
+paintDOM()
